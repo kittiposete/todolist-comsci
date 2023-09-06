369c06c8bcce5a6b576badc1bce21f10bd7db25f
kittiposete <96778671+kittiposete@users.noreply.github.com>
2023-09-06 14:15:57 +0700

    Add date attribute to TaskItem class

--- task_item.py
+++ task_item.py
@@ -1,4 +1,5 @@
 class TaskItem:
-    def __init__(self, task_title, is_important):
+    def __init__(self, task_title, is_important, date: int):
         self.task_title = task_title
         self.is_important = is_important
+        self.date = date
