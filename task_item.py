67992a86785b9a5bdd9af82167eb297032c64345
kittiposete <96778671+kittiposete@users.noreply.github.com>
2023-09-11 09:06:58 +0700

    add is_finished field to task_item class

--- task_item.py
+++ task_item.py
@@ -1,5 +1,6 @@
 class TaskItem:
-    def __init__(self, task_title, is_important, date: int):
+    def __init__(self, task_title, is_important, date: int, is_finished=False):
         self.task_title = task_title
         self.is_important = is_important
         self.date = date
+        self.is_finished = is_finished
